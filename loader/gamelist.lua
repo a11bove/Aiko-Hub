af72bda1fec6f1c14b13f1b46489ab208d2cc0f5
a11bove <chiyomlbb25@gmail.com>
2025-07-07 02:48:26 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -1,6 +1,6 @@
 local Games = {
   [126884695634066] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoGag.lua", -- Grow a Garden
-  [109983668079237] = "https://github.com/a11bove/Aiko-Hub/raw/refs/heads/main/loader/scripts/aikoSab.lua" -- Steal a Brainrot
+  [109983668079237] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoSab.lua" -- Steal a Brainrot
 }
 
 return Games
