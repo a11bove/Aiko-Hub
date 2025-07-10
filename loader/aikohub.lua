9d64d656b3972277e1d9f7df6b74665e941bd989
a11bove <chiyomlbb25@gmail.com>
2025-07-10 21:13:50 +0800

    Update aikohub.lua

--- loader/aikohub.lua
+++ loader/aikohub.lua
@@ -1,6 +1,6 @@
 local gs = loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/gamelist.lua"))()
 
-for PlaceID, Execute in pairs(Games) do
+for PlaceID, Execute in pairs(gs) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
     end
