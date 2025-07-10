f354962a1d0f838079adb0fb6d823f02a90def27
a11bove <chiyomlbb25@gmail.com>
2025-07-10 23:33:25 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -1,6 +1,8 @@
 local gs = {
   [126884695634066] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoGag.lua", -- Grow a Garden Place2
   [91867617264223] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoGag.lua", -- Grow a Garden Place1
+--[116495829188952] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoDr.lua", -- Dead Rails Place1
+--[70876832253163] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoDr.lua", -- Dead Rails Place2
   [99567941238278] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoIg.lua", -- Ink Game Place1
 }
 
