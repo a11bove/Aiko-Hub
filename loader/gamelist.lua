e313805a99458140844d4926ee57e01ea5932f1f
a11bove <chiyomlbb25@gmail.com>
2025-07-24 02:29:04 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -5,6 +5,8 @@ local gs = {
   [109983668079237] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoSab.lua", -- Steal a Brainrot Place2
   [99567941238278] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoIg.lua", -- Ink Game Place1
   [125009265613167] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoIg.lua", -- Ink Game Place2
+  [79546208627805] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in the Forest Place1
+  [126509999114328] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in the Forest Place2
 }
 
 return gs
