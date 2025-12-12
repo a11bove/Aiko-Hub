19b0c575ca21285632822cf9892d42311f952337
a11bove <chiyomlbb25@gmail.com>
2025-12-12 23:25:06 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -5,7 +5,7 @@ local gs = {
   -- [109983668079237] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoSab.lua", -- Steal a Brainrot Place2
   -- [79546208627805] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in the Forest Place1
   -- [126509999114328] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in the Forest Place2
-  [131667667758514] = "", -- Diesel 'N Steel
+  [131667667758514] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoDns.lua", -- Diesel 'N Steel
 }
 
 return gs
