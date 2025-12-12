012575cff3f504881a8386ffd121a7d6d8153753
a11bove <chiyomlbb25@gmail.com>
2025-12-12 20:03:20 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -3,10 +3,9 @@ local gs = {
   [91867617264223] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoGag.lua", -- Grow a Garden Place1
   [96342491571673] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoSab.lua", -- Steal a Brainrot Place1
   [109983668079237] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoSab.lua", -- Steal a Brainrot Place2
-  [99567941238278] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoIg.lua", -- Ink Game Place1
-  [125009265613167] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoIg.lua", -- Ink Game Place2
   [79546208627805] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in the Forest Place1
   [126509999114328] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in the Forest Place2
+  [131667667758514] = "", -- Diesel 'N Steel
 }
 
 return gs
