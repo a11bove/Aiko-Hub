1752bdb7b0e8a4f2a6d00382257e18d99fff8ffc
a11bove <chiyomlbb25@gmail.com>
2025-12-12 23:39:08 +0800

    Fix URL for The Forge script in gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -1,6 +1,6 @@
 local gs = {
   [131667667758514] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoDns.lua", -- Diesel 'N Steel
-  [7655890409] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoTheForge", -- The Forge
+  [7655890409] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoTheForge.lua", -- The Forge
 }
 
 return gs
