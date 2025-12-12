c01f544dd6520a93ff2738ce98fd018cef9122e5
a11bove <chiyomlbb25@gmail.com>
2025-12-12 23:37:24 +0800

    Clean up commented game entries in gamelist.lua
    Removed commented-out game entries from the list.

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -1,11 +1,6 @@
 local gs = {
-  -- [126884695634066] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoGag.lua", -- Grow a Garden Place2
-  -- [91867617264223] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoGag.lua", -- Grow a Garden Place1
-  -- [96342491571673] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoSab.lua", -- Steal a Brainrot Place1
-  -- [109983668079237] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoSab.lua", -- Steal a Brainrot Place2
-  -- [79546208627805] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in the Forest Place1
-  -- [126509999114328] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in the Forest Place2
   [131667667758514] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoDns.lua", -- Diesel 'N Steel
+  [7655890409] = "", -- The Forge
 }
 
 return gs
