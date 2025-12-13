7cc0535953795338d2d89960082c0902f98af134
a11bove <chiyomlbb25@gmail.com>
2025-12-13 21:15:47 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -1,6 +1,6 @@
 local gs = {
   [131667667758514] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoDns.lua", -- Diesel 'N Steel
-  [7655890409] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoTheForge.lua", -- The Forge
+  [7671049560] = "https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/scripts/aikoTheForge.lua", -- The Forge
   [7326934954] = "", -- 99 Nights in The Forest
 }
 
