25c659a80eda9e9cd238b86dfeb6064366101be5
a11bove <chiyomlbb25@gmail.com>
2025-12-13 21:40:34 +0800

    Update aikoDns.lua

--- loader/scripts/aikoDns.lua
+++ loader/scripts/aikoDns.lua
@@ -135,7 +135,7 @@ local Minimizer = Fluent:CreateMinimizer({
 local Options = Fluent.Options
 
 do
-    local Section = Tabs.Main:AddSection(Title = "Exploit", "star")
+    local Section = Tabs.Main:AddSection("Exploit", "star")
 
     Tabs.Main:AddParagraph({
         Icon = "rbxassetid://140356301069419",
@@ -160,7 +160,7 @@ do
         autoFarmKm(value)
     end)
 
-    local Section = Tabs.Main:AddSection(Title = "Visual", "eye")
+    local Section = Tabs.Main:AddSection("Visual", "eye")
     
     Tabs.Main:AddParagraph({
         Icon = "rbxassetid://140356301069419",
