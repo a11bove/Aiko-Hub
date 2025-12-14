ee7f699fa90e47b7eb6ace752bdd2dbb1f46c657
a11bove <chiyomlbb25@gmail.com>
2025-12-14 13:50:36 +0800

    Update aiko99NITF.lua

--- loader/scripts/aiko99NITF.lua
+++ loader/scripts/aiko99NITF.lua
@@ -1,1 +1,9 @@
-temp
+_G.State = {}
+_G.Config = {}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/nitf/camp.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/nitf/ui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/nitf/combat.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/nitf/esp.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/nitf/fly.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/nitf/tp.lua"))()
