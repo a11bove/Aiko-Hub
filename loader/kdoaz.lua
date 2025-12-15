913f282e95ac2693d3d9bc1300cbb9ea7ca904d9
a11bove <chiyomlbb25@gmail.com>
2025-12-16 06:57:22 +0800

    Delete loader/kdoaz.lua

--- loader/kdoaz.lua
+++
@@ -1,7 +0,0 @@
-local gs = loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/gamelist.lua"))()
-
-for PlaceID, Execute in pairs(gs) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
-    end
-end
