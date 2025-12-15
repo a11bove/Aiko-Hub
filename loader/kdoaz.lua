88828dbdea19751288e1b7e0a8ee7af2a10ca44d
a11bove <chiyomlbb25@gmail.com>
2025-12-16 01:45:27 +0800

    Update kdoaz.lua

--- loader/kdoaz.lua
+++ loader/kdoaz.lua
@@ -1,4 +1,4 @@
-local gs = loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/Aiko-Hub/refs/heads/main/loader/gamelist.lua"))()
+local gs = loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/gamelist.lua"))()
 
 for PlaceID, Execute in pairs(gs) do
     if PlaceID == game.PlaceId then
