6a67f1f8a2609cfadbac47b91babaa35fa4d8c68
a11bove <chiyomlbb25@gmail.com>
2025-12-16 14:38:23 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -1,6 +1,7 @@
+--["131667667758514"] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoDns.lua", -- Diesel 'N 
 local gs = {
-  [7671049560] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoTheForge.lua", -- The Forge
   [7326934954] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in The Forest
-    ["131667667758514"] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoDns.lua", -- Diesel 'N Steel
+  [7671049560] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoTheForge.lua", -- The Forge
+  -- Add the GameId for Diesel 'N Steel here when you find it
 }
 return gs
