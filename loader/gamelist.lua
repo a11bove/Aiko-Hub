67739f8db4720de4ec27d3c23f84829ba8979cf6
a11bove <chiyomlbb25@gmail.com>
2025-12-17 00:24:10 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -2,5 +2,6 @@ local gs = {
   [7326934954] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in The Forest
   [7671049560] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoTheForge.lua", -- The Forge
   [6466802953] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoDns.lua", -- Diesel 'N Steel
+  [8316902627] = "", -- Fish It
 }
 return gs
