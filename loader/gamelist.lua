fb326404a9e9bd80baa162b892dba8b02f0f9273
a11bove <chiyomlbb25@gmail.com>
2025-12-17 17:50:09 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -3,6 +3,6 @@ local gs = {
   [7671049560] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoTheForge.lua", -- The Forge
   [6466802953] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoDns.lua", -- Diesel 'N Steel
   [6701277882] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoFishIt.lua", -- Fish It (GameId)
-  [16732694052] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoFishIt.lua", -- Fish It (PlaceId)
+  [121864768012064] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoFishIt.lua", -- Fish It (PlaceId)
 }
 return gs
